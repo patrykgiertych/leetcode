549453251fd8c1da4b6588c5a4368eeaa6997e9e
patrykgiertych <patrykgiertych1@gmail.com>
2021-11-01 09:53:26 +0100

    concatenation of array

---
+++ src/concatenation_of_array/Solution.java
@@ -0,0 +1,14 @@
+package concatenation_of_array;
+
+public class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
+}
