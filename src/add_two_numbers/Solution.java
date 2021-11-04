fea800b7e14322fbcdf7d08773d0ed644ecb4ab7
patrykgiertych <patrykgiertych1@gmail.com>
2021-11-04 15:52:06 +0100

    add two numbers

---
+++ src/add_two_numbers/Solution.java
@@ -0,0 +1,46 @@
+package add_two_numbers;
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultList = new ListNode(0);
+        ListNode resultIterator = resultList;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+
+        boolean carry = false;
+
+        while (p1 != null || p2 != null) {
+            int sum = 0;
+            if (p1 == null) {
+                sum += p2.val;
+                p2 = p2.next;
+            } else if (p2 == null) {
+                sum += p1.val;
+                p1 = p1.next;
+            } else {
+                sum += p1.val + p2.val;
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            if (carry) {
+                sum++;
+            }
+
+            if (sum >= 10) {
+                sum %= 10;
+                carry = true;
+            } else {
+                carry = false;
+            }
+
+            ListNode newNode = new ListNode(sum);
+            resultIterator.next = newNode;
+            resultIterator = resultIterator.next;
+        }
+
+        if (carry) resultIterator.next = new ListNode(1);
+
+        return resultList.next;
+    }
+}
