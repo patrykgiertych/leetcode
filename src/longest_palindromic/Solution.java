a3630bcdeed5c0c365c7198e2ee068aff3228d25
patrykgiertych <patrykgiertych1@gmail.com>
2021-11-07 14:00:31 +0100

    longest palindromic substring

---
+++ src/longest_palindromic/Solution.java
@@ -0,0 +1,34 @@
+package longest_palindromic;
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i +1);
+
+            int len = Math.max(len1,len2);
+            if (len > end - start) {
+                start = i - ((len - 1) / 2);
+                end = i + len / 2;
+            }
+
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public int expand(String sub, int left, int right) {
+        if (sub== null || left > right) return 0;
+
+        while (right < sub.length() && left >= 0 && sub.charAt(left) == sub.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
