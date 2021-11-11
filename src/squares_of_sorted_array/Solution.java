d6b373d58a700679466cd35d7058068109051d8f
patrykgiertych <patrykgiertych1@gmail.com>
2021-11-11 13:54:27 +0100

    squares of sorted array

---
+++ src/squares_of_sorted_array/Solution.java
@@ -0,0 +1,13 @@
+package squares_of_sorted_array;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
