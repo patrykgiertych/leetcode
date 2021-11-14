5cb8d5224cfc9f4b0b7eccc6c368fa233ed331c5
patrykgiertych <patrykgiertych1@gmail.com>
2021-11-14 23:07:09 +0100

    binary search

--- src/binary_search/Solution.java
+++ src/binary_search/Solution.java
@@ -2,8 +2,12 @@ package binary_search;
 
 class Solution {
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) return i;
+        int mid, left = 0, right = nums.length - 1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
         }
         return -1;
     }
